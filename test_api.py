f82112eca55a7194a32306d0e1fc46429acb4fe5
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 17:06:15 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -18,7 +18,7 @@ request_data = request_bod.json()
 
 
 # from itertools import filter
-developer = [elem for elem in filter(lambda x: x['developer_group_id'] == '429726001', request_data)]
+developer = [elem for elem in filter(lambda x: x['developer_group_id'] == '429726001', request_data)][]
 # [elem for elem in request_data if elem['developer_group_id'] == 429726001]
 # request_data[0]
 developer = request_data[200]
