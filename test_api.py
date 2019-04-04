cf30ea59cc88f67875d750cdc9287b2c37e24789
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 18:04:40 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -29,7 +29,7 @@ for developer in request_developers:
 
 developer_bod = requests.get(url=DETAILED_DEVELOPER_ENDPOINT, headers=headers)
 developer_data = developer_bod.json()
-developer_data = [dict(dev_data, **{k, v for k, v in request_data[0].iteritems() if k == "developer_group_id")} for dev_data in developer_data]
+developer_data = [dict(dev_data, **{k, v for k: v in request_data[0].iteritems() if k == "developer_group_id")} for dev_data in developer_data]
 
 
     for key,val in developer_data.iteritems():
