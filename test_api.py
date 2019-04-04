8c0b823529971f8bda7fc4f600153c4433cd6075
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 16:33:21 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -2,9 +2,9 @@ import requests
 from fake_useragent import UserAgent
 ua = UserAgent()
 
-API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects"
+# API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects"
 API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/dim_developer_group"
-
+# 81.177.103.102:443
 
 headers = requests.utils.default_headers()
 headers['User-Agent'] = ua.random
