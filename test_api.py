b5a4e206edf2ffd4ace471a2df17959b931f449c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 16:32:27 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -1,2 +1,28 @@
 import requests
-import 
+from fake_useragent import UserAgent
+ua = UserAgent()
+
+
+#
+# PARAMS = {"r": entry["reporter_id"],
+#                   "px": entry["classif"],
+#                   "ps": entry["year"],
+#                   "p": "all",
+#                   "rg": entry["regime"],
+#                   "cc": entry["agg_level"],
+#                   "max": 250000,
+#                   "fmt": "json",
+#                   "type": "C",
+#                   "freq": "A",
+#                   "head": "M"
+#                   }
+API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects"
+API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/dim_developer_group"
+
+
+headers = requests.utils.default_headers()
+headers['User-Agent'] = ua.random
+
+request_bod = requests.get(url=API_ENDPOINT, headers=headers)
+len(request_bod.json())
+# https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects
