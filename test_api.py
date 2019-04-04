ffd2c96904fecc49eed9c857b2ac8e648e0f275a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 17:00:02 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -19,6 +19,10 @@ request_data = request_bod.json()
 request_data[0]
 
 DETAILED_DEVELOPER_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/аналитика/grapi/v1/developer_group_region?developerGroupId={developer_id}".format(**{"developer_id" : request_data[0]["developer_group_id"]})
+DETAILED_DEVELOPER_ENDPOINT
+
+request_bod = requests.get(url=API_ENDPOINT, headers=headers)
+request_data = request_bod.json()
 
 # len(request_bod.json())
 #
