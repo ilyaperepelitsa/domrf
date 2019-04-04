418e0bdbcb866e12bd6ffa8c13a3dd34541498d3
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 17:02:00 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -16,6 +16,7 @@ headers['User-Agent'] = ua.random
 request_bod = requests.get(url=API_ENDPOINT, headers=headers)
 request_data = request_bod.json()
 
+429726001
 developer = request_data[200]
 
 DETAILED_DEVELOPER_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/аналитика/grapi/v1/developer_group_region?developerGroupId={developer_id}".format(**{"developer_id" : developer["developer_group_id"]})
