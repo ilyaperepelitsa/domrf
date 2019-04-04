00ffc17b1393be3e664184b84e94271b194f5da2
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 16:56:04 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -6,7 +6,7 @@ ua = UserAgent()
 # API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/аналитика/grapi/v1/geoObjects"
 API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/аналитика/grapi/v1/dim_developer_group"
 
-https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/developer_group_region?developerGroupId=429726001
+https://xn--80az8a.xn--d1aqf.xn--p1ai/аналитика/grapi/v1/developer_group_region?developerGroupId=429726001
 
 # 81.177.103.102:443
 
