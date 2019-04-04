0a607297de42f95f393eeb0e895da4f5c9f28726
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 18:19:01 +0300

    Updated data.csvtest_api.py .

--- test_api.py
+++ test_api.py
@@ -34,7 +34,7 @@ for developer in request_developers:
     developer_data = developer_bod.json()
     developer_data = [dict(dev_data, **{k: v for k, v in request_data[0].items() if k == "developer_group_id"}) for dev_data in developer_data]
 
-    with open('data.csv', 'ab') as fp:
+    with open('data.csv', 'a') as fp:
         for developer_datum in developer_data:
             a = csv.writer(fp, delimiter=',')
             a.writerows(developer_datum)
