75fb33771a2595f4f32e7576275eae52e2a9941e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 16:32:32 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -2,20 +2,6 @@ import requests
 from fake_useragent import UserAgent
 ua = UserAgent()
 
-
-#
-# PARAMS = {"r": entry["reporter_id"],
-#                   "px": entry["classif"],
-#                   "ps": entry["year"],
-#                   "p": "all",
-#                   "rg": entry["regime"],
-#                   "cc": entry["agg_level"],
-#                   "max": 250000,
-#                   "fmt": "json",
-#                   "type": "C",
-#                   "freq": "A",
-#                   "head": "M"
-#                   }
 API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects"
 API_ENDPOINT = "https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/dim_developer_group"
 
