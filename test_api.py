b283418186a1c1bcac6a255e2855416254fdd40b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-04 16:41:57 +0300

    Updated test_api.py.

--- test_api.py
+++ test_api.py
@@ -12,4 +12,6 @@ headers['User-Agent'] = ua.random
 
 request_bod = requests.get(url=API_ENDPOINT, headers=headers)
 len(request_bod.json())
+
+pd.DataF
 # https://xn--80az8a.xn--d1aqf.xn--p1ai/%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0/grapi/v1/geoObjects
