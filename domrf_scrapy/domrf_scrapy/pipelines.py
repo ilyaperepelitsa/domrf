3fdbc30bb97a5a081fbb71e1c30e377f68de56ef
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 02:09:44 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -33,7 +33,7 @@ class DomrfScrapyPipeline(object):
         yield item
 
 
-class DomrfScrapyPipeline(object):
+class Developer(object):
     def process_item(self, item, spider):
 
         item["developer_group_id"] = int(item["developer_group_id"][0])
