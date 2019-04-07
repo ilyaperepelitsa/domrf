d1aa9ebfeb62bd1a9da3b06d48e1d2a82e9bbf72
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 02:24:27 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -79,8 +79,8 @@ class DataPipeline(object):
                             "endDate" : item["developer_group_name"],
                             "total_living_floor_size" : item["developer_group_name"],
                             "appt_num" : item["developer_group_name"],
-                            "region_id" : item["developer_group_name"],
-                            "region_id" : item["developer_group_name"],
+                            "object_count" : item["developer_group_name"],
+                            "total_living_floor_size_pct" : item["developer_group_name"],
                             "region_id" : item["developer_group_name"]}
 
         data_exists = session_test.query(exists().where(and_(
