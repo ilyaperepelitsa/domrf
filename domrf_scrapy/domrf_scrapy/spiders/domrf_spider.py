bc5916ee9453269ffa5bec7aee21d73300217ea7
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 22:31:30 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -82,8 +82,9 @@ class DomrfSpiderSpider(scrapy.Spider):
         developer_group_address = response.meta['developer_group_address']
         developer_report = json.loads(response.body_as_unicode())["payload"]
 
-        return {"developer" : type(developer),
-                    @
+        return {
+                "developer" : type(developer),
+                #list
                 "developer_data" : type(developer_data),
                 "developer_group_address" : type(developer_group_address),
                 "developer_report" : type(developer_report)}
