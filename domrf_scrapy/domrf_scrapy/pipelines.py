fed515710c8cb325ec9ce8f13d26106911e87afa
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 01:58:26 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from news.models import *
 
 class DomrfScrapyPipeline(object):
     def process_item(self, item, spider):
