1d68b1cde2d16e036e066db8725caef24cd617cf
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 02:14:54 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -40,7 +40,8 @@ class DeveloperPipeline(object):
                             "developer_group_name" : item["developer_group_name"],
                             "developer_group_address" : item["developer_group_address"]}
 
-        developer_exists = session_test.query(exists().where(and_(User.name == 'leela', User.fullname == 'leela dharan'))).scalar()
+        developer_exists = session_test.query(exists().where(and_(
+                    User.name == 'leela', User.fullname == 'leela dharan'))).scalar()
         filter(and_(User.name == 'leela', User.fullname == 'leela dharan'))
         if not url_exists:
             adding_url = Url_entry(**url_entry)
