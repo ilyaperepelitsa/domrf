33024f7ed76304a29f2a4afeeef04fb57505ea33
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 21:11:14 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -4,8 +4,8 @@ import scrapy
 
 class DomrfSpiderSpider(scrapy.Spider):
     name = 'domrf_spider'
-    allowed_domains = ['наш.дом.рф']
-    start_urls = ['https://наш.дом.рф/аналитика/grapi/v1/dim_developer_group']
+    # allowed_domains = ['наш.дом.рф']
+    # start_urls = ['https://наш.дом.рф/аналитика/grapi/v1/dim_developer_group']
 
     def parse(self, response):
         # yield {"pew" : dir(response)}
