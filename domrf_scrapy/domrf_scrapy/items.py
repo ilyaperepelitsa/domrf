77ab1834dab22c9b2361bc5db5dec01f96f2057f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 23:03:52 +0300

    Updated domrf_spider.cpython-36.pyc.

--- domrf_scrapy/domrf_scrapy/items.py
+++ domrf_scrapy/domrf_scrapy/items.py
@@ -6,9 +6,9 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
-
-class DomrfScrapyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class FercItem(scrapy.Item):
+    file_urls = Field()
+    files = Field()
     pass
