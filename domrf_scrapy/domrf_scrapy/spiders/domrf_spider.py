fca6a0da92f1e2f508b95ca61e2f670f1c114131
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 21:04:11 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -8,4 +8,4 @@ class DomrfSpiderSpider(scrapy.Spider):
     start_urls = ['https://наш.дом.рф/аналитика/grapi/v1/dim_developer_group']
 
     def parse(self, response):
-        yield response
+        yield dir(response)
