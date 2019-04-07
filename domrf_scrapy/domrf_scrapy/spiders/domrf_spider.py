b36ef2128af91cf67a7d559178927596105d1f2a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 22:03:02 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -35,7 +35,7 @@ class DomrfSpiderSpider(scrapy.Spider):
     def parse_list(self, response):
         for developer in json.loads(response.body_as_unicode()):
             developer_details_request =  Request(
-                "https://наш.дом.рф/аналитика/grapi/v1/developer_group_region?developerGroupId={developer_id}",
+                "https://наш.дом.рф/аналитика/grapi/v1/developer_group_region?developerGroupId={developer_id}".format(**{"developer_id" : developer["developer_group_id"]}),
                 meta={'developer_main': developer}
                 headers = self.headers,
                 callback=self.parse_list
