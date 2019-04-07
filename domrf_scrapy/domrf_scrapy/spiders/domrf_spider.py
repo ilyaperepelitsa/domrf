c5fbf46ae7b3806e607e7b4c4fa67d3ee215c56e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 21:32:19 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -21,6 +21,8 @@ class DomrfSpiderSpider(scrapy.Spider):
     def parse(self, response):
         PR = Request(
             "https://наш.дом.рф/аналитика/grapi/v1/dim_developer_group",
-            callback=self.parse_дшые
+            callback=self.parse_list
         )
         return PR
+
+    def parse(self, response):
