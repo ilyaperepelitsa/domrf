560c4e42749cdb41c8bc6f46aadd49e95963c358
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 02:35:14 +0300

    Updated query_db.py.

--- domrf_scrapy/domrf_scrapy/query_db.py
+++ domrf_scrapy/domrf_scrapy/query_db.py
@@ -1,4 +1,4 @@
 # domrf_scrapy
 from domrf_scrapy.domrf_scrapy.models import *
 
-docker run --name domrf -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker run --name domrf -e POSTGRES_PASSWORD=mysecretpassword POSTGRES_USER -d postgres
