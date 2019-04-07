69764d9ec975857ded7b59bb17bad95a8f931f99
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 01:59:09 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from news.models import *
+from sqlalchemy.sql import select
 
 class DomrfScrapyPipeline(object):
     def process_item(self, item, spider):
