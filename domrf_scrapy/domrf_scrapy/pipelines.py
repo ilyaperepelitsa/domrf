212c2b6ffeaa749455a482d79e9d4cd9c40862ef
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 02:09:58 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -36,21 +36,12 @@ class DomrfScrapyPipeline(object):
 class DeveloperPipeline(object):
     def process_item(self, item, spider):
 
-        item["developer_group_id"] = int(item["developer_group_id"][0])
-        item["developer_group_name"] = item["developer_group_name"][0]
-        item["developer_group_address"] = item["developer_group_address"][0]
 
-        item["region_id"] = int(item["region_id"][0])
-        item["region_name"] = item["region_name"][0]
+        yield item
 
-        item["startDate"] = item["startDate"][0]
-        item["endDate"] = item["endDate"][0]
 
-        item["total_living_floor_size"] = int(item["total_living_floor_size"][0])
-        item["appt_num"] = int(item["appt_num"][0])
-        item["object_count"] = int(item["object_count"][0])
-        item["total_living_floor_size_pct"] = float(item["total_living_floor_size_pct"][0])
-        item["typed_volume_pct"] = float(item["typed_volume_pct"][0])
-        item["rating"] = int(item["rating"][0])
+class DeveloperPipeline(object):
+    def process_item(self, item, spider):
+
 
         yield item
