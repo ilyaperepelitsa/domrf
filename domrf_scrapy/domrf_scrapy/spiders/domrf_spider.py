76486b961d0351dff70da8d8054137de50c4f5dd
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 21:54:44 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -8,7 +8,11 @@ class DomrfSpiderSpider(scrapy.Spider):
     name = 'domrf_spider'
     # allowed_domains = ['наш.дом.рф']
     start_urls = ['https://наш.дом.рф/аналитика/grapi/v1/dim_developer_group']
-    
+    headers = {"Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Connection": "keep-alive",
+                "content-type": "application/json"}
     #
     # def parse(self, response):
     #
@@ -23,7 +27,7 @@ class DomrfSpiderSpider(scrapy.Spider):
     def parse(self, response):
         PR = Request(
             "https://наш.дом.рф/аналитика/grapi/v1/dim_developer_group",
-            headers = ,
+            headers = self.headers,
 
             callback=self.parse_list
         )
