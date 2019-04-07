9f0af2a1b5e3366586aae79c01f4ad346f2454d9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 21:23:07 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -8,13 +8,12 @@ class DomrfSpiderSpider(scrapy.Spider):
     # start_urls = ['https://наш.дом.рф/аналитика/grapi/v1/dim_developer_group']
 
     def parse(self, response):
-        # yield {"pew" : dir(response)}
-        def parse(self, response):
-    for i in range(1,3,1):
-        PR = Request(
-            'http://myproxyapi.com',
-            headers=self.headers,
-            meta={'newrequest': Request('htp//sitetoscrape.com',  headers=self.headers),},
-            callback=self.parse_PR
-        )
-        yield PR
+        
+        for i in range(1,3,1):
+            PR = Request(
+                'http://myproxyapi.com',
+                headers=self.headers,
+                meta={'newrequest': Request('htp//sitetoscrape.com',  headers=self.headers),},
+                callback=self.parse_PR
+            )
+            yield PR
