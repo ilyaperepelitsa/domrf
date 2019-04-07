705e6ac6d41dade27625efafb3c0ae8dc9de7aa8
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 01:19:38 +0300

    Updated models.py.

--- domrf_scrapy/domrf_scrapy/models.py
+++ domrf_scrapy/domrf_scrapy/models.py
@@ -50,7 +50,7 @@ class Developer(Base_item):
 
 class DeveloperData(Base_item):
     __tablename__ = "authors"
-    developer_group_id = Column(INTEGER, primary_key = True)
+    developer_group_id = Column(INTEGER, primary_key = TrueForeignKey("urls.url_id"), nullable=False)
     author_name = Column(VARCHAR(50), unique = True)
 
     def __repr__(self):
