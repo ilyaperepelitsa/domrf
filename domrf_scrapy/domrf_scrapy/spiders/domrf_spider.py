6607e5fe12f2b552e85b15d5584f795e0136392b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 21:54:54 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -30,7 +30,7 @@ class DomrfSpiderSpider(scrapy.Spider):
             headers = self.headers,
             callback=self.parse_list
         )
-        yield PR
+        return PR
 
     def parse_list(self, response):
         return {"pew" : json.loads(response.body_as_unicode())}
