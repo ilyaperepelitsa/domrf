c0cd5f1f59aaedc32fe7cb7aaa49b18f13d8585b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 02:17:39 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -56,8 +56,21 @@ class DeveloperPipeline(object):
 class RegionPipeline(object):
     def process_item(self, item, spider):
 
+developer_entry = {"developer_group_id" : item["developer_group_id"],
+                    "developer_group_name" : item["developer_group_name"],
+                    "developer_group_address" : item["developer_group_address"]}
 
-        yield item
+developer_exists = session_test.query(exists().where(and_(
+            Developer.developer_group_id == developer_entry['developer_group_id'],
+            Developer.developer_group_name == developer_entry['developer_group_name'],
+            Developer.developer_group_address == developer_entry['developer_group_address']))).scalar()
+
+if not developer_exists:
+    adding_developer = Developer(**developer_entry)
+    session_test.add(adding_developer)
+    session_test.commit()
+
+yield item
 
 class DataPipeline(object):
     def process_item(self, item, spider):
