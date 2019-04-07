ff95f2aa68452a93c7b31fa8578b0917ba59619c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 02:06:00 +0300

    Updated pipelines.py.

--- domrf_scrapy/domrf_scrapy/pipelines.py
+++ domrf_scrapy/domrf_scrapy/pipelines.py
@@ -17,6 +17,13 @@ class DomrfScrapyPipeline(object):
         item["url"] = item["url"][0]
         item["url"] = item["url"][0]
         item["url"] = item["url"][0]
+        item["url"] = item["url"][0]
+        item["url"] = item["url"][0]
+        item["url"] = item["url"][0]
+        item["url"] = item["url"][0]
+        item["url"] = item["url"][0]
+        item["url"] = item["url"][0]
+        item["url"] = item["url"][0]
         
 
         developer_group_id = Field()
