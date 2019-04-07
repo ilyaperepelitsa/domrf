6fe68795b0427d35a93f186029c6da4474f235d0
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-07 22:04:19 +0300

    Updated domrf_spider.py.

--- domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
+++ domrf_scrapy/domrf_scrapy/spiders/domrf_spider.py
@@ -45,3 +45,4 @@ class DomrfSpiderSpider(scrapy.Spider):
         # return {"pew" : json.loads(response.body_as_unicode())}
 
     def parse_developer_detailed(self, response):
+        newrequest = response.meta['newrequest']
