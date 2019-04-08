060e9567de776fae82cef513d9760751ab5dfc77
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 16:10:11 +0300

    Updated models_psql.py.

--- domrf_scrapy/domrf_scrapy/models_psql.py
+++ domrf_scrapy/domrf_scrapy/models_psql.py
@@ -98,6 +98,6 @@ Session_test = sessionmaker(bind = engine_test)
 session_test = Session_test()
 
 
-q = session_test.query(Region).all()
-# session_test.rollback()
-q
+# q = session_test.query(Region).all()
+# # session_test.rollback()
+# q
