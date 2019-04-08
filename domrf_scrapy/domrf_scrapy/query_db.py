a46c6cda92c7e811dc66962161842e18287f7c9d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 13:11:43 +0300

    Updated query_db.py.

--- domrf_scrapy/domrf_scrapy/query_db.py
+++ domrf_scrapy/domrf_scrapy/query_db.py
@@ -1,18 +1,7 @@
 # domrf_scrapy
 from domrf_scrapy.domrf_scrapy.models_test import *
 import os
-# # docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -e POSTGRES_USER=vtbuser -d postgres
-# docker run --name domrf -e POSTGES_USER=vtbuser -e POSTGRES_PASSWORD=qwerty123 -e POSTGRES_DB=domrf postgres
-#
-# docker run --name postgres1 --network postgres-network -e POSTGRES_PASSWORD=qwerty123 -d postgres
-#
-# docker network create --driver bridge postgres-network
-# docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -d postgres
-#
-#
-#
-# docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword postgres
-#
+
 import pandas as pd
 
 def inst_to_dict(inst, delete_id=True):
@@ -27,7 +16,7 @@ q = (session_test.query(DeveloperData, Developer, Region)
             .join(Developer, DeveloperData.developer_group_id == Developer.developer_group_id)
             .join(Region, DeveloperData.region_id == Region.region_id)
             .all())
-q
+# q
 
 test_df = pd.concat([pd.DataFrame(list(map(lambda x: inst_to_dict(x[0]), q))),
                                 pd.DataFrame(list(map(lambda x: inst_to_dict(x[1]), q))),
