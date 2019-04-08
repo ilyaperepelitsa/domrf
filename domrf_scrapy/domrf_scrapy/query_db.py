987a72c23a636a2b58bb2c710fb85a8773e5a1d6
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 12:57:24 +0300

    Updated query_db.py.

--- domrf_scrapy/domrf_scrapy/query_db.py
+++ domrf_scrapy/domrf_scrapy/query_db.py
@@ -1,17 +1,17 @@
 # domrf_scrapy
-from domrf_scrapy.domrf_scrapy.models import *
+from domrf_scrapy.domrf_scrapy.models_test import *
 
-# docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -e POSTGRES_USER=vtbuser -d postgres
-docker run --name domrf -e POSTGES_USER=vtbuser -e POSTGRES_PASSWORD=qwerty123 -e POSTGRES_DB=domrf postgres
+# # docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -e POSTGRES_USER=vtbuser -d postgres
+# docker run --name domrf -e POSTGES_USER=vtbuser -e POSTGRES_PASSWORD=qwerty123 -e POSTGRES_DB=domrf postgres
+#
+# docker run --name postgres1 --network postgres-network -e POSTGRES_PASSWORD=qwerty123 -d postgres
+#
+# docker network create --driver bridge postgres-network
+# docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -d postgres
+#
+#
+#
+# docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword postgres
+#
 
-docker run --name postgres1 --network postgres-network -e POSTGRES_PASSWORD=qwerty123 -d postgres
-
-docker network create --driver bridge postgres-network
-docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -d postgres
-
-
-
-docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword postgres
-
-
-q = (session_test.query(Region).all())
+q = session_test.query(Region).all()
