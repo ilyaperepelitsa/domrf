58086818ca467d86d2bbdda509ff3c9ab8c465e3
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 13:00:42 +0300

    Updated query_db.py.

--- domrf_scrapy/domrf_scrapy/query_db.py
+++ domrf_scrapy/domrf_scrapy/query_db.py
@@ -13,8 +13,22 @@ from domrf_scrapy.domrf_scrapy.models_test import *
 #
 # docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword postgres
 #
+import pandas as pd
+
+def inst_to_dict(inst, delete_id=True):
+    dat = {}
+    for column in inst.__table__.columns:
+        dat[column.name] = getattr(inst, column.name)
+    # if delete_id:
+    #     dat.pop("num")
+    return dat
 
 q = (session_test.query(DeveloperData, Developer, Region)
             .join(Developer, DeveloperData.developer_group_id == Developer.developer_group_id)
-            .join(Region, DeveloperData.reporter == Reporter.id)
+            .join(Region, DeveloperData.region_id == Region.region_id)
             .all())
+q
+
+test_df = pd.concat([pd.DataFrame(list(map(lambda x: inst_to_dict(x[0]), q))),
+                                pd.DataFrame(list(map(lambda x: inst_to_dict(x[1]), q))),
+                                pd.DataFrame(list(map(lambda x: inst_to_dict(x[2]), q)))], axis = 1)
