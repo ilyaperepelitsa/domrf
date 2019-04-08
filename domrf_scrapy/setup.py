166ee87d5a73cbb026d3d6ef5941ab6f941692fd
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 17:29:58 +0300

    Updated setup.py.

---
+++ domrf_scrapy/setup.py
@@ -0,0 +1,21 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name =          'domrf_scrapy',
+    version =       '0.1',
+    packages =      find_packages(exclude=("apply",)),
+    # packages = ["dataslap_jobs", "credentials"],
+    # package_dir = {
+    #                 "credentials" : "credentials",
+    #                 "news" : "news"},
+    # package_data = {"credenitals" : ["credenitals/passwords.json"]},
+    # zip_safe = False,
+    zip_safe = True,
+    # package_data = {"credenitals" : ["credenitals/passwords.json"]},
+    # package_data = {("credenitals", ["credenitals/passwords.json"])},
+
+    include_package_data=True,
+    entry_points =  {'scrapy': ['settings = dataslap_jobs.settings']}
+)
