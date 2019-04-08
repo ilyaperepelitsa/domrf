e6273265b5c32739020015eb0019fb0d8a868b00
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 12:58:05 +0300

    Updated query_db.py.

--- domrf_scrapy/domrf_scrapy/query_db.py
+++ domrf_scrapy/domrf_scrapy/query_db.py
@@ -14,4 +14,14 @@ from domrf_scrapy.domrf_scrapy.models_test import *
 # docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword postgres
 #
 
-q = session_test.query(Region).all()
+q = (session_test.query(DeveloperData, Developer, Region)
+            .filter(and_(Trade_aggregation_entry.reporter == query_params["reporter_id"],
+                        Trade_aggregation_entry.trade_regime == query_params["trade_regime_id"],
+                        Trade_aggregation_entry.classification == query_params["classification"],
+                        Trade_aggregation_entry.aggregation_level == query_params["aggregation_level"]))
+            .join(Trade_regime, Trade_aggregation_entry.trade_regime == Trade_regime.id)
+            .join(Reporter, Trade_aggregation_entry.reporter == Reporter.id)
+            .join(Partner, Trade_aggregation_entry.partner == Partner.id)
+            .join(Quantity_code, Trade_aggregation_entry.quantity_code == Quantity_code.id)
+            .join(Commodity_code_EN, Trade_aggregation_entry.commodity == Commodity_code_EN.id)
+            .all())
