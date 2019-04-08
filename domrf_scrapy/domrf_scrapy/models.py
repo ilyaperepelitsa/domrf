9ba0c2267acf359067ac19a197baea7395d06ed8
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 11:53:27 +0300

    Updated models.py.

--- domrf_scrapy/domrf_scrapy/models.py
+++ domrf_scrapy/domrf_scrapy/models.py
@@ -28,7 +28,8 @@ from sqlalchemy.sql import exists
 # dataslap_postgres = keys.get_dataslap_postgres()
 
 
-engine_test = create_engine('postgres://postgres:mysecretpassword@0.0.0.0:5432/domrf')
+# engine_test = create_engine('postgres://postgres:mysecretpassword@0.0.0.0:5432/domrf')
+engine_test = create_engine('sqlite:///domrf.db')
 # engine_test = create_engine('postgres://postgres:qwerty123@localhost:5432/domrf')
 Base_item = declarative_base()
 
