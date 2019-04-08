c1e1e1bf7ebaa6ce974430b1bf0073a51dc59413
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-04-08 10:41:16 +0300

    Updated query_db.py.

--- domrf_scrapy/domrf_scrapy/query_db.py
+++ domrf_scrapy/domrf_scrapy/query_db.py
@@ -3,5 +3,7 @@ from domrf_scrapy.domrf_scrapy.models import *
 
 # docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -e POSTGRES_USER=vtbuser -d postgres
 docker run postgres -e 'POSTGES_USER=vtbuser' -e 'POSTGRES_PASSWORD=qwerty123' -e 'POSTGRES_DB=domrf'
+docker run --name postgres1 --network postgres-network -e POSTGRES_PASSWORD=qwerty123 -d postgres
 
+docker network create --driver bridge postgres-network
 docker run --name domrf -e POSTGRES_PASSWORD=qwerty123 -d postgres
